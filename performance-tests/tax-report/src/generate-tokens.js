0c4c11477771918afb2d4d284e74e5ec2643b509
Dagfinn Olsen <dagfinno@gmail.com>
2024-09-27 11:11:22 +0200

    Test reachability (#89)
    * added test to check if we can reach altinn-apis from github
    
    * added action
    
    * added http-debug
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * added verbose
    
    * stop on fault
    
    * stop on fault
    
    * stop on fault
    
    * stop on fault
    
    * stop on fault
    
    * stop on fault
    
    * stop on fault
    
    * stop on fault
    
    * stop on fault
    
    * added data.csv
    
    * No valid text file?
    
    * added data.csv again
    
    * added extra html report, increased vus and duration
    
    * Increased setup timeout
    
    * Shorten data file
    
    * do token gen in separate step
    
    * install xk6
    
    * added generate-tokens and newer version of golang image
    
    * call correct k6 script in generate token step
    
    * share files between steps
    
    * setting working directory
    
    * fixing path
    
    * cleaning up
    
    * scaling up test
    
    * exporting reports to web
    
    * full path of bash
    
    * use sh instead of bash
    
    * use sh instead of bash
    
    * usinig actions/setup-go
    
    * usinig actions/setup-go
    
    * usinig actions/setup-go
    
    * testing self-hosted runner
    
    * testing self-hosted runner
    
    * testing self-hosted runner on windows img
    
    * refactoring
    
    * refactoring
    
    * Only run one iteration when pushing
    
    * added actions again
    
    * added actions again
    
    * action for scheduled and ondemand execution
    
    * Selecting runner
    
    * Selecting runner
    
    * Selecting runner
    
    * changing push rules
    
    * checking if push filters works
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct
    
    * trying to get the runner-selection correct

---
+++ performance-tests/tax-report/src/generate-tokens.js
@@ -0,0 +1,37 @@
+import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
+import { SharedArray } from 'k6/data';
+import {generateToken} from './token-generator.js';
+import file from 'k6/x/file';
+
+
+const environment = __ENV.env.toLowerCase();
+const tokenGeneratorUserName = __ENV.tokengenuser;
+const tokenGeneratorUserPwd = __ENV.tokengenuserpwd;
+const limit = (__ENV.limit === undefined ? 0 : __ENV.limit);
+
+const filepath = 'data-with-tokens.csv';
+const idKeys = new SharedArray('idKeys', function () {
+  return papaparse.parse(open('data.csv'), { header: true }).data;
+});
+
+export const options = { 
+  vus: 1,
+};
+
+export default function(data) {
+  file.writeString(filepath, 'userId,partyId,ssn,token');
+  var count = 0
+  for (const idKey of idKeys) {
+    var tokenGenParams = {
+      env: environment,
+      userId: idKey.userid,
+      partyId: idKey.partyid,
+      pid: idKey.ssn,
+      ttl: 3600*24*10,
+    };
+    var token = generateToken(tokenGeneratorUserName, tokenGeneratorUserPwd, tokenGenParams);
+    file.appendString(filepath, `\n${idKey.userid},${idKey.partyid},${idKey.ssn},${token}`);
+    count += 1;
+    if (count >= limit && limit > 0) break;
+  };
+}
