134ad6c6f886b53b286aadf418d75418a1b35e25
Dagfinn Olsen <dagfinno@gmail.com>
2024-10-10 15:21:35 +0200

    first running version that creates a dialog and reads dialogs

--- performance-tests/dialogporten/src/search-graphql.js
+++ performance-tests/dialogporten/src/search-graphql.js
@@ -2,99 +2,44 @@ import http from 'k6/http';
 import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
 import { randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 import { SharedArray } from 'k6/data';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import {buildQueryParametersForEndpoint} from '../../generate-tokens/src/token-generator.js';
+
+const env = __ENV.env;
+const filepath = `../data/.tmp/data-with-tokens-${env}.csv`;
 
 const idKeys = new SharedArray('idKeys', function () {
-  return papaparse.parse(open('data-with-tokens.csv'), { header: true }).data;
+  return papaparse.parse(open(filepath), { header: true }).data;
 });
 
-const subscription_key = __ENV.subscription_key;
-const env = __ENV.env;
+const baseUrl = `https://platform.${env}.altinn.no/dialogporten/api/v1/enduser/dialogs`
 
 export const options = {
-  discardResponseBodies: true,
+  //discardResponseBodies: true,
   summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'p(99.5)', 'p(99.9)', 'count'],
   thresholds: {
     http_req_failed: ['rate<0.01'],
   },
-
 };
 
-export function getUrl(env) {
-    var url;
-    switch(env) {
-        case 'tt01':
-            url = '';
-            break;
-        case 'at21':
-            url = '';
-            break;
-        case 'yt01':
-            url = '';
-            break;
-        default:
-            url = '';
-            break;
-    }
-}
-
-export function setup() {
-  var data = {
-    searchUrl: getUrl(env),
-    idKeys: []
-  };
-
-  for (const idKey of idKeys) {
-    data.idKeys.push({
-      partyId: idKey.partyId, 
-      userId: idKey.userId,
-      ssn: idKey.ssn,
-      token: idKey.token
-    });
-    if ((options.vus === undefined || options.vus === 1) && (options.iterations === undefined || options.iterations === 1)) {
-      break;
-    }
-  };
-  return data;
-}
-
-export default function(data) {
+export default function() {
   if ((options.vus === undefined || options.vus === 1) && (options.iterations === undefined || options.iterations === 1)) {
-    search(data, data.idKeys[0]);
+    search(idKeys[0]);
   }
   else {
-    while (true) { search(data, randomItem(data.idKeys)); }
+    while (true) { search(randomItem(idKeys)); }
   }
 }
 
-export function search(data, id) {
-  var query = 
-    {
-      Language: 'nb',
-      InstanceOwnerPartyIdList: [ id.partyId ],
-      FromCreated: new Date(2018, 12, 12),
-      ToCreated: new Date(2099, 1, 1),
-      IncludeActive: false,
-      IncludeArchived: true
-    };
-
+export function search(id) {
   var params = {
     headers: {
-      Authorization: 'Bearer ' + id.token,
-      'Content-Type': 'application/json',
-      'Ocp-Apim-Subscription-Key': subscription_key
+      Authorization: 'Bearer ' + id.idtoken,
     },
   }
 
-  var request_body = JSON.stringify(query)
-  http.post(data.searchUrlYt, request_body, params);
-}
-
-export function handleSummary(data) {
-  return {
-    'stdout': textSummary(data, { indent: ' ', enableColors: true }),
-    'stdout.txt': textSummary(data, { indent: ' ', enableColors: true }),
-    "summary.html": htmlReport(data),
-  };
+  var queryParams = { ServiceResource: `urn:altinn:resource:${id.resource}` };
+  var uri = baseUrl + buildQueryParametersForEndpoint(queryParams);
+  var resp = http.get(uri, params);
+  var json_resp = resp.json();
+  console.log(json_resp.items[0].createdAt);
 }
